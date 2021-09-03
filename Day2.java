3d8cc4b584c0ab503b9fbc468b64112b61ba7aa3
OmNikam04 <80584774+OmNikam04@users.noreply.github.com>
2021-09-03 11:21:11 +0530

    Day 2 Assignment Submission

---
+++ Day2.java
@@ -0,0 +1,9 @@
+// Todo: Take two arguments from command line and print sum of them 
+
+public class Day2 {
+    public static void main(String[] args) {
+        int a =Integer.parseInt(args[0]);
+        int b =Integer.parseInt(args[1]);
+        System.out.println("Hello the addition of a and b is: "+(a+b));
+    }
+}
